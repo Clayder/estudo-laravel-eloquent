ad61d605d4eeb6b958990fa13b4a72cd3f584409
Clayder <peterclayder@gmail.com>
2017-12-17 14:35:58 -0200

    criei as constantes (TYPE_INDIVIDUAL, TYPE_LEGAL, MARITAL_STATUS) da classe Clients e configurei o atributo fillable.

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clients extends Model
 {
-    //
-}
+    const TYPE_INDIVIDUAL = 'individual';
+    const TYPE_LEGAL = 'legal';
+
+    const MARITAL_STATUS = [
+        1 => 'Solteiro',
+        2 => 'Casado',
+        3 => 'Divorciado'
+    ];
+
+    protected $fillable = [
+            'name',
+            'document_number',
+            'email',
+            'phone',
+            'defaulter',
+            'date_birth',
+            'sex',
+            'marital_status',
+            'physical_disability',
+            'company_name',
+            'client_type'
+        ];
+    }
