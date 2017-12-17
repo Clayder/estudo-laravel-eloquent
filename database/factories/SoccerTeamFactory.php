16ffd3cfdff3f425313d2df505d2e95b9844e29f
Clayder <peterclayder@gmail.com>
2017-12-17 14:51:38 -0200

    configurei a factory SoccerTimeFactory

--- database/factories/SoccerTeamFactory.php
+++ database/factories/SoccerTeamFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        // Todos os times vão ter nome de rua
+        'name' => $faker->streetName,
     ];
 });
