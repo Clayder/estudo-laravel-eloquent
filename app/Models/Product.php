b269fb7cbe82445bb11dc1df38e3abf794694945
Clayder <peterclayder@gmail.com>
2017-12-19 14:05:31 -0200

    Criei a factory de categorias

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name'];
+
+    /**
+     * Realizo o relacionamento many-to-many com a tabela categories.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
